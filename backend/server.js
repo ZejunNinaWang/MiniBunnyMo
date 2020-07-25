60fca99c399d45f28cfa91de3f7a0598d57e2f98
Zejun Wang <wangzejunnina@gmail.com>
2020-07-25 10:59:56 -0400

    Edited server js to serve static assets if in production

--- backend/server.js
+++ backend/server.js
@@ -88,4 +88,9 @@ app.get('/api/image/:filename', (req, res) => {
 //config server to serve files inside uploads folder
 // app.use('/uploads', express.static(path.join(__dirname, '/../uploads')));
 
+app.use(express.static(path.join(__dirname, '/../frontend/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(`${__dirname}/../frontend/build/index.html`));
+});
+
 app.listen(config.PORT, () => {console.log("Server started at http://localhost:5000")});
